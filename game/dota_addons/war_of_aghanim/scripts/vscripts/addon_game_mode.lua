678f72e1f6e862d2b3f85fb0cfaaf97eab4e9a45
ghost <yandongpeng@qq.com>
2017-01-19 13:37:23 +0800

    move the codes position

--- game/dota_addons/war_of_aghanim/scripts/vscripts/addon_game_mode.lua
+++ game/dota_addons/war_of_aghanim/scripts/vscripts/addon_game_mode.lua
@@ -241,3 +241,34 @@ function CAddonAdvExGameMode:OnNPCSpawn(keys)
 	--attacker:AddItemByName("item_ultimate_scepter")
 end
 
+function CAddonAdvExGameMode:OnHeroInGame(hero)
+  print("[DOTA2IMBA] Hero spawned in game for first time -- " .. hero:GetUnitName())
+
+  --[[ Multiteam configuration, currently unfinished
+  local team = "team1"
+  local playerID = hero:GetPlayerID()
+  if playerID > 3 then
+    team = "team2"
+  end
+  print("setting " .. playerID .. " to team: " .. team)
+  MultiTeam:SetPlayerTeam(playerID, team)]]
+
+  -- This line for example will set the starting gold of every hero to 500 unreliable gold
+  hero:SetGold(3000, false)
+  local level = hero:GetLevel()
+      while level < 6 do
+        hero:AddExperience (2000,0,false,false)
+        level = hero:GetLevel()
+      end
+
+  -- These lines will create an item and add it to the player, effectively ensuring they start with the item
+  --local item = CreateItem("item_multiteam_action", hero, hero)
+  --hero:AddItem(item)
+
+  --[[ --These lines if uncommented will replace the W ability of any hero that loads into the game
+    --with the "example_ability" ability
+  local abil = hero:GetAbilityByIndex(1)
+  hero:RemoveAbility(abil:GetAbilityName())
+  hero:AddAbility("example_ability")]]
+end
+
